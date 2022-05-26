e4dc3a857d9c37d38247a29be025b91f74f00421
LuisDiego1010 <74754000+LuisDiego1010@users.noreply.github.com>
2022-05-26 04:04:11 -0600

    Methods for class Data Base

--- Interface.py
+++ Interface.py
@@ -1,6 +1,8 @@
+import re
 from tkinter import *
 import tkinter as tk
 import shelve
+from tkinter import messagebox
 
 #Clase para almacenar toda la información de cada estudiante
 class Student():
@@ -20,7 +22,7 @@ class Student():
         return self.carnet
         
     def set_course(self,course):
-        self.course=course[0]
+        self.course={course:0}
     
     def get_course(self):
         return self.course
@@ -62,6 +64,25 @@ class Data_Base():
                 return True
             else: 
                 return False
+    
+    def found_carnet(self,carnet):
+        with shelve.open(self.archive, writeback=True) as students:
+            if str(carnet) in students:
+                return True
+            else: 
+                return False
+    
+    def found_course(self,carnet,course):
+        with shelve.open(self.archive, writeback=True) as students:
+            if str(course) in (students[str(carnet)]["Cursos"]):
+                return True
+            else:
+                False
+            
+    def add_course(self,carnet,course):
+        with shelve.open(self.archive, writeback=True) as students:
+            course_temp=students[carnet]["Cursos"][course]=0
+            print(course_temp)
 
     def set_information(self,students_list):
         with shelve.open(self.archive, writeback=True) as students:
@@ -71,18 +92,84 @@ class Data_Base():
             direction=students_list[3]
             telephone=students_list[4]
             email=students_list[5]
-            students[carnet]= {"Name":name,"Courses":courses,"Direction":direction,"Telephone: ":telephone,"Email: ":email}
+            students[carnet]= {"Nombre":name,"Cursos":courses,"Direccion":direction,"Telefono":telephone,"Email":email}
             print(students)
+        students.close()
     
     def get_students(self):
-        cadena = ""
+        text = ""
         with shelve.open(self.archive, writeback=True) as students:
             if self.busy()==True:
                 for i,j in students.items():
-                    cadena += "\nEstudiante = " + i +":"
+                    text += "\n\nEstudiante = " + i +":"
                     for f,z in j.items():
-                        cadena += "\t" + f + ":" + z
-        return cadena
+                        text += "\n" + str(f) + ":" + str(z) 
+        students.close()
+        return text
+    
+    def get_student(self,carnet):
+        with shelve.open(self.archive, writeback=True) as students:
+            text=""
+            for i,j in students.items():
+                if i == str(carnet):
+                    text+="La información del estudiante con carnet "+ i + " es: \n"
+                    for f,z in j.items():
+                        text += str(f) + ":" + str(z) + "\n"
+        students.close()
+        return text
+
+    def get_courses(self,carnet):
+        with shelve.open(self.archive, writeback=True) as students:
+            characters="{}"
+            text=" "
+            for i,j in students.items():
+                if i == str(carnet):
+                    print("Los cursos de",i, "son: ")
+                    conversion=str(j["Cursos"])
+                    for x in range(len(characters)):
+                        conversion = conversion.replace(characters[x],"")
+                    text += "Los cursos de " + i + " son: \n"+conversion.replace(",","\n")
+        students.close()
+        return text            
+    
+    def set_information(self,carnet,name,direction,telephone,email):
+        with shelve.open(self.archive, writeback=True) as students:
+            print(len(name))
+            print(len(direction))
+            print(len(telephone))
+            print(len(email))
+
+            if len(name)!=0:
+                print("ACTUALIZADO EL NOMBRE")
+                for i,j in students.items():
+                    if i == str(carnet):
+                        j["Nombre"]=name
+                        print("ACTUALIZADO EL NOMBRE")
+
+            if len(direction)!=0:
+                for i,j in students.items():
+                    if i == str(carnet):
+                        j["Direccion"]=direction
+
+            if len(telephone)!=0:
+                for i,j in students.items():
+                    if i == str(carnet):
+                        j["Telefono"]=telephone
+
+            if len(email)!=0:
+                for i,j in students.items():
+                    if i == str(carnet):
+                        j["Email"]=email
+
+    def set_calification(self,carnet,course,new_calification):    
+        with shelve.open(self.archive, writeback=True) as students:
+            if str(course) in (students[str(carnet)]["Cursos"]):
+                for i,j in students.items():
+                    for k,l in j["Cursos"].items():
+                        if i==str(carnet) and k == str(course):
+                            j["Cursos"][k] = new_calification
+
+
 
 class App(tk.Tk):
     #Constructor of class App
@@ -149,7 +236,7 @@ class App(tk.Tk):
         self.title("App Create Student")
         title = Label (self,text="Favor llene los espacios con la información solicitada:", font=("Adobe Gothic Std B",24),background="white").place(x=200,y=15)
         lbl_carnet = Label (self,text="Digite el carnet del estudiante: ", font=("Adobe Gothic Std B",16),background="white").place(x=50,y=75)
-        ent_carnet= Entry(self)
+        ent_carnet= Entry(self,font=("Adobe Gothic Std B",16),width=35)
         ent_carnet.place(x=370,y=75)
         lbl_course = Label (self,text="Digite el curso que desea agregar al estudiante: ", font=("Adobe Gothic Std B",16),background="white").place(x=50,y=135)
         ent_course= Entry(self,font=("Adobe Gothic Std B",16),width=35)
@@ -167,7 +254,7 @@ class App(tk.Tk):
         ent_email= Entry(self,font=("Adobe Gothic Std B",16),width=35)
         ent_email.place(x=364,y=375)
 
-        def guardar():
+        def save():
             self.student.set_carnet(ent_carnet.get())
             self.student.set_course(ent_course.get())
             self.student.set_name(ent_name.get())
@@ -176,7 +263,7 @@ class App(tk.Tk):
             self.student.set_email(ent_email.get())
             self.db.set_information(self.student.get_information())
 
-        guardar = Button(self,text="Guardar",font=('Arial 20'),command=guardar).place(x=100,y=500)
+        save = Button(self,text="GUARDAR",font=('Arial 20'),command=save).place(x=100,y=500)
         self.mainloop()
 
     def Create_course_app(self):
@@ -184,9 +271,26 @@ class App(tk.Tk):
         self.title("App Create Course")
         title = Label (self,text="Favor llene los espacios con la información solicitada:", font=("Adobe Gothic Std B",24),background="white").place(x=200,y=15)
         lbl_carnet = Label (self,text="Digite el carnet del estudiante: ", font=("Adobe Gothic Std B",16),background="white").place(x=50,y=75)
-        ent_carnet= Entry(self,font=("Adobe Gothic Std B",16),width=35).place(x=370,y=75)
-        lbl_course = Label (self,text="Digite el curso que desea agregar al estudiante: ", font=("Adobe Gothic Std B",16),background="white").place(x=50,y=135)
-        ent_course= Entry(self,font=("Adobe Gothic Std B",16),width=35).place(x=535,y=135)
+        ent_carnet= Entry(self,font=("Adobe Gothic Std B",16),width=35)
+        ent_carnet.place(x=370,y=75)
+        def found():
+            if  self.db.found_carnet(ent_carnet.get()):   
+                lbl_course = Label (self,text="Digite el curso que desea agregar al estudiante: ", font=("Adobe Gothic Std B",16),background="white").place(x=50,y=135)
+                ent_course= Entry(self,font=("Adobe Gothic Std B",16),width=35)
+                ent_course.place(x=535,y=135)
+                def found_course():
+                    if self.db.found_course(ent_carnet.get(),ent_course.get()):
+                        messagebox.showinfo(title="Error", message="El curso ya se encuentra en la base de datos " + " del estudiante " + ent_carnet.get())
+                    else:
+                        self.db.add_course(ent_carnet.get(),ent_course.get())
+                        messagebox.showinfo(title="Éxito", message="El curso " + str(ent_course.get()) + " fue agregado correctamente" + "al estudiante " + str(ent_carnet.get()))
+                found_course = Button(self,text="AGREGAR",height=1,width=12,font=('Arial 9'),command=found_course).place(x=970,y=137)
+            else:
+                messagebox.showinfo(title="Error", message="El carnet "+ent_carnet.get() + " no se encuentra en la base de datos ")
+                
+        found = Button(self,text="BUSCAR",height=1,width=10,font=('Arial 9'),command=found).place(x=810,y=78)
+
+        self.mainloop()
 
     def Read_studens_app(self):
         self=App()
@@ -206,50 +310,98 @@ class App(tk.Tk):
         self.title("App Read Student")
         title = Label (self,text="Favor ingrese el carnet del estudiante del que desea ver la información", font=("Adobe Gothic Std B",24),background="white").place(x=10,y=3)
         lbl_carnet = Label (self,text="Digite el carnet del estudiante: ", font=("Adobe Gothic Std B",16),background="white").place(x=50,y=75)
-        ent_carnet= Entry(self,font=("Adobe Gothic Std B",16),width=35).place(x=370,y=75)
-        #COLOCAR UN MESSAGEBOX QUE INDIQUE SI EL CARNET SE ENCUENTRA EN EL ARCHIVO
-        text_student = tk.Text(self, height=15, width=140)
-        text_student.pack(side=tk.LEFT)
-        long_text = "Primer estudiante: "
-        text_student.insert(tk.END, long_text)
+        ent_carnet= Entry(self,font=("Adobe Gothic Std B",16),width=35)
+        ent_carnet.place(x=370,y=75)
+        def found():
+            if  self.db.found_carnet(ent_carnet.get()):
+                text_student = tk.Text(self, height=15, width=140,font=('Arial 16'))
+                scroll_bar = tk.Scrollbar(self)
+                scroll_bar.pack(side=tk.RIGHT)
+                text_student.pack(side=tk.LEFT)
+                long_text = str(self.db.get_student(ent_carnet.get()))
+                text_student.insert(tk.END, long_text)
+            else:
+                messagebox.showinfo(title="Error", message="El carnet "+ ent_carnet.get() + " no se encuentra en la base de datos ")
+        found = Button(self,text="BUSCAR",height=1,width=10,font=('Arial 9'),command=found).place(x=800,y=75)
 
     def Read_courses_app(self):
         self=App()
         self.title("App Read Courses")
-        title = Label (self,text="Favor ingrese el carnet del estudiante del que desea ver la información", font=("Adobe Gothic Std B",24),background="white").place(x=10,y=3)
+        title = Label (self,text="Favor ingrese el carnet del estudiante del que desea ver la información de los cursos matriculados", font=("Adobe Gothic Std B",24),background="white").place(x=10,y=3)
         lbl_carnet = Label (self,text="Digite el carnet del estudiante: ", font=("Adobe Gothic Std B",16),background="white").place(x=50,y=75)
-        ent_carnet= Entry(self,font=("Adobe Gothic Std B",16),width=35).place(x=370,y=75)
-        #COLOCAR UN MESSAGEBOX QUE INDIQUE SI EL CARNET SE ENCUENTRA EN EL ARCHIVO
-        text_student = tk.Text(self, height=15, width=140)
-        text_student.pack(side=tk.LEFT)
-        long_text = "Los cursos del estudiante son: "
-        text_student.insert(tk.END, long_text)
+        ent_carnet= Entry(self,font=("Adobe Gothic Std B",16),width=35)
+        ent_carnet.place(x=370,y=75)
+        def found():
+            if  self.db.found_carnet(ent_carnet.get()):
+                text_student = tk.Text(self, height=15, width=140,font=('Arial 16'))
+                scroll_bar = tk.Scrollbar(self)
+                scroll_bar.pack(side=tk.RIGHT)
+                text_student.pack(side=tk.LEFT)
+                long_text = str(self.db.get_courses(ent_carnet.get()))
+                text_student.insert(tk.END, long_text)
+            else:
+                messagebox.showinfo(title="Error", message="El carnet "+ ent_carnet.get() + " no se encuentra en la base de datos ")
+        found = Button(self,text="BUSCAR",height=1,width=10,font=('Arial 9'),command=found).place(x=800,y=75)
     
     def Update_information_app(self):
         self=App()
         self.title("App Update Information")
         title = Label (self,text="Llene los espacios con la información que desea actualizar", font=("Adobe Gothic Std B",24),background="white").place(x=10,y=3)
         lbl_carnet = Label (self,text="Digite el carnet del estudiante al que desea cambiarle la información: ", font=("Adobe Gothic Std B",16),background="white").place(x=50,y=75)
-        ent_carnet= Entry(self,font=("Adobe Gothic Std B",16),width=35).place(x=690,y=75)
-        lbl_name = Label (self,text="Digite el nombre del estudiante: ", font=("Adobe Gothic Std B",16),background="white").place(x=50,y=195)
-        ent_name= Entry(self,font=("Adobe Gothic Std B",16),width=35).place(x=384,y=195)
-        lbl_direction = Label (self,text="Digite la dirección del estudiante: ", font=("Adobe Gothic Std B",16),background="white").place(x=50,y=255)
-        ent_direction= Entry(self,font=("Adobe Gothic Std B",16),width=35).place(x=399,y=255)
-        lbl_telephone = Label (self,text="Digite el telefono del estudiante: ", font=("Adobe Gothic Std B",16),background="white").place(x=50,y=315)
-        ent_telephone= Entry(self,font=("Adobe Gothic Std B",16),width=35).place(x=390,y=315)
-        lbl_email = Label (self,text="Digite el email del estudiante: ", font=("Adobe Gothic Std B",16),background="white").place(x=50,y=375)
-        ent_email= Entry(self,font=("Adobe Gothic Std B",16),width=35).place(x=364,y=375)
+        ent_carnet= Entry(self,font=("Adobe Gothic Std B",16),width=15)
+        ent_carnet.place(x=750,y=75)
+        def found():
+            if  self.db.found_carnet(ent_carnet.get()):
+                lbl_name = Label (self,text="Digite el nombre del estudiante: ", font=("Adobe Gothic Std B",16),background="white").place(x=50,y=195)
+                ent_name= Entry(self,font=("Adobe Gothic Std B",16),width=35)
+                ent_name.place(x=384,y=195)
+                lbl_direction = Label (self,text="Digite la dirección del estudiante: ", font=("Adobe Gothic Std B",16),background="white").place(x=50,y=255)
+                ent_direction= Entry(self,font=("Adobe Gothic Std B",16),width=35)
+                ent_direction.place(x=399,y=255)
+                lbl_telephone = Label (self,text="Digite el telefono del estudiante: ", font=("Adobe Gothic Std B",16),background="white").place(x=50,y=315)
+                ent_telephone= Entry(self,font=("Adobe Gothic Std B",16),width=35)
+                ent_telephone.place(x=390,y=315)
+                lbl_email = Label (self,text="Digite el email del estudiante: ", font=("Adobe Gothic Std B",16),background="white").place(x=50,y=375)
+                ent_email= Entry(self,font=("Adobe Gothic Std B",16),width=35)
+                ent_email.place(x=364,y=375)
+                def set_information():
+                    self.db.set_information(ent_carnet.get(),ent_name.get(),ent_direction.get(),ent_telephone.get(),ent_email.get())
+                    messagebox.showinfo(title="Éxito", message="La informacion del estudiante "+ent_carnet.get() + " fue actualizada ")
+                set_information = Button(self,text="ACTUALIZAR",height=1,width=12,font=('Arial 9'),command=set_information).place(x=760,y=450)
+            else:
+                messagebox.showinfo(title="Error", message="El carnet "+ ent_carnet.get() + " no se encuentra en la base de datos ")
+        found = Button(self,text="BUSCAR",height=1,width=10,font=('Arial 9'),command=found).place(x=950,y=77)
+
     
     def Update_calification_app(self):
         self=App()
         self.title("App Update Calification")
         title = Label (self,text="Llene los espacios con la información solicitada", font=("Adobe Gothic Std B",24),background="white").place(x=10,y=3)
         lbl_carnet = Label (self,text="Digite el carnet del estudiante al que desea cambiarle la nota: ", font=("Adobe Gothic Std B",16),background="white").place(x=50,y=75)
-        ent_carnet= Entry(self,font=("Adobe Gothic Std B",16),width=35).place(x=620,y=75)
-        lbl_course = Label (self,text="Digite el curso del estudiante al que desea cambiarle la nota: ", font=("Adobe Gothic Std B",16),background="white").place(x=50,y=195)
-        ent_course = Entry(self,font=("Adobe Gothic Std B",16),width=35).place(x=635,y=195)
-        lbl_new_calification = Label (self,text="Digite la nueva nota que se la asignara al curso anterior: ", font=("Adobe Gothic Std B",16),background="white").place(x=50,y=255)
-        ent_new_calification = Entry(self,font=("Adobe Gothic Std B",16),width=35).place(x=595,y=255)
+        ent_carnet= Entry(self,font=("Adobe Gothic Std B",16),width=15)
+        ent_carnet.place(x=680,y=75)
+        def found():
+            if  self.db.found_carnet(ent_carnet.get()):   
+                lbl_course = Label (self,text="Digite el curso que desea agregar al estudiante: ", font=("Adobe Gothic Std B",16),background="white").place(x=50,y=135)
+                ent_course= Entry(self,font=("Adobe Gothic Std B",16),width=35)
+                ent_course.place(x=535,y=135)
+                def found_course():
+                    if self.db.found_course(ent_carnet.get(),ent_course.get()):
+                        lbl_calification = Label (self,text="Digite la nota que desea asignarle al curso ", font=("Adobe Gothic Std B",16),background="white").place(x=50,y=195)
+                        ent_calification = Entry(self,font=("Adobe Gothic Std B",16),width=15)
+                        ent_calification.place(x=500,y=197)
+                        def set_calification():
+                            self.db.set_calification(ent_carnet.get(),ent_course.get(),ent_calification.get())
+                        set_calification = Button(self,text="ACTUALIZAR",height=1,width=12,font=('Arial 9'),command=set_calification).place(x=760,y=300)
+                        messagebox.showinfo(title="Éxito", message="La nota del curso "+ent_course.get() + " fue actualizada ")
+
+                    else:
+                        messagebox.showinfo(title="Error", message="El curso " + str(ent_course.get()) + " no se encuentra agregado al estudiante " + str(ent_carnet.get()))
+                found_course = Button(self,text="BUSCAR CURSO",height=1,width=12,font=('Arial 9'),command=found_course).place(x=970,y=137)
+            else:
+                messagebox.showinfo(title="Error", message="El carnet "+ent_carnet.get() + " no se encuentra en la base de datos ")
+                
+        found = Button(self,text="BUSCAR",height=1,width=10,font=('Arial 9'),command=found).place(x=880,y=78)
 
     def Delete_student(self):
         self=App()
@@ -300,3 +452,5 @@ def main_App():
     app.mainloop()
 
 main_App()
+
+db =Data_Base()
